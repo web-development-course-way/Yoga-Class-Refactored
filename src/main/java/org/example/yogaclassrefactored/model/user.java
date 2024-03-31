489f97cd637533903a8ea2a1ea523c6431ac217a
Ahmad Al-Deeb <ahmeddeeb22222@gmail.com>
2024-03-31 21:04:30 +0200

    Add abstract class user

---
+++ src/main/java/org/example/yogaclassrefactored/model/user.java
@@ -0,0 +1,86 @@
+package org.example.yogaclassrefactored.model;
+
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@MappedSuperclass
+public abstract class user {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "phone", unique = true)
+    private String phone;
+
+    @Column(name = "email", unique = true)
+    private String email;
+
+    @Column(name = "nationality")
+    private String nationality;
+
+    @Column(name = "birth_date")
+    private String birthDate;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
+}
