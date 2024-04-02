c0f4b059ca2eb58ee1f365859182ba4e5afcfa99
Andrew Seif <Andrew.Seif@outlook.com>
2024-04-02 16:20:08 +0200

    Added Object Mapper

--- src/main/java/com/yoga/horus/dtoMapper/UserMapper.java
+++ src/main/java/com/yoga/horus/dtoMapper/UserMapper.java
@@ -4,11 +4,10 @@ import com.yoga.horus.dto.UserDTO;
 import com.yoga.horus.entity.User;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
-import org.springframework.stereotype.Service;
+
 
 @Mapper (componentModel = "spring")
 public interface UserMapper {
-    UserMapper user = Mappers.getMapper(UserMapper.class);
     UserDTO userToUserDTO(User user);
     User userDTOToUser(UserDTO userDTO);
 
