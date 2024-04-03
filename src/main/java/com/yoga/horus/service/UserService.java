0dab3ef4e8975b3150d42ff029a1edbc038daf92
Ahmad Al-Deeb <ahmeddeeb22222@gmail.com>
2024-04-03 04:57:29 +0200

    Fix typo to follow naming convention

--- src/main/java/com/yoga/horus/service/UserService.java
+++ src/main/java/com/yoga/horus/service/UserService.java
@@ -1,10 +1,9 @@
 package com.yoga.horus.service;
 
-import com.yoga.horus.constant.Constant;
 import com.yoga.horus.dto.UserDTO;
 import com.yoga.horus.dtoMapper.UserMapper;
 import com.yoga.horus.entity.User;
-import com.yoga.horus.repository.UsersRepository;
+import com.yoga.horus.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,17 +14,17 @@ import java.util.stream.Collectors;
 @Service
 public class UserService implements BaseService <User,UserDTO>{
 
-    private final UsersRepository usersRepository;
+    private final UserRepository userRepository;
     private final UserMapper userMapper;
 
-    public UserService(UsersRepository usersRepository, UserMapper userMapper) {
-        this.usersRepository = usersRepository;
+    public UserService(UserRepository userRepository, UserMapper userMapper) {
+        this.userRepository = userRepository;
         this.userMapper = userMapper;
     }
 
     @Override
     public List<UserDTO> getAll() {
-        return usersRepository.findAll()
+        return userRepository.findAll()
                 .stream()
                 .map(userMapper::userToUserDTO)
                 .collect(Collectors.toList());
@@ -33,7 +32,7 @@ public class UserService implements BaseService <User,UserDTO>{
 
     @Override
     public UserDTO getOne(UUID id) {
-        return usersRepository.findById(id)
+        return userRepository.findById(id)
                 .map(userMapper::userToUserDTO)
                 .get();
     }
@@ -42,14 +41,14 @@ public class UserService implements BaseService <User,UserDTO>{
 
     @Override
     public UserDTO create(User user) {
-        User newUser = usersRepository.save(user);
+        User newUser = userRepository.save(user);
         return userMapper.userToUserDTO(newUser);
     }
 
     @Override
     public UserDTO update(UUID id, User user) {
-       if (usersRepository.existsById(id)){
-           User savedUser = usersRepository.save(user);
+       if (userRepository.existsById(id)){
+           User savedUser = userRepository.save(user);
            return userMapper.userToUserDTO(savedUser);
        } else {
            throw new NoSuchElementException();
@@ -58,8 +57,8 @@ public class UserService implements BaseService <User,UserDTO>{
 
     @Override
     public void delete(UUID id) {
-        if (usersRepository.existsById(id)){
-            usersRepository.deleteById(id);
+        if (userRepository.existsById(id)){
+            userRepository.deleteById(id);
         } else {
             throw new NoSuchElementException();
         }
