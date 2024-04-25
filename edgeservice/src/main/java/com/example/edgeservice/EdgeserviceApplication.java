d89061e95fd2808419cd142c2b2a7a3ae95f0e76
Andrew Seif <Andrew.Seif@outlook.com>
2024-04-25 02:06:46 +0200

    Add keycloak integration with gw and authentication with ds services

--- edgeservice/src/main/java/com/example/edgeservice/EdgeserviceApplication.java
+++ edgeservice/src/main/java/com/example/edgeservice/EdgeserviceApplication.java
@@ -3,9 +3,13 @@ package com.example.edgeservice;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.reactive.EndpointRequest;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
@@ -21,6 +25,15 @@ public class EdgeserviceApplication {
 	@Bean
 	KeyResolver keyResolver() {
 		return exchange -> Mono.just("ANONYMOUS");
+	}
+
+	@Bean
+	SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+		return http
+				.authorizeExchange(exchange -> exchange.matchers(EndpointRequest.toAnyEndpoint()).permitAll()
+						.anyExchange().authenticated())
+				.oauth2Login(Customizer.withDefaults())
+				.build();
 	}
 
 }
